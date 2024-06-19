4ac3eea58aa1e6038d3f4277a29202129ed38299
Bryan <>
2024-06-19 14:52:30 +0800

    New (Variables) : Added Variables

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,57 @@
 fun main() {
+//  Printing
     println("Hello World")
+    val line: String = "--------------------------------------------------------------------------"
+
+//  Variables
+    val characterValue: Char = 'A'       // Character
+    val stringValue: String = "String"   // String
+    val booleanVale: Boolean = false     // Boolean
+    val integerValue: Int = 0            // Integer
+    val doubleValue: Double = 0.0        // Double
+    val byteValue: Byte = 0              // Byte
+    val shortValue: Short = 0            // Short
+    val longValue: Long = 0              // Long
+    val floatValue: Float = 0.0F         // Float
+
+//  Max Values for Integer Data Types
+    val maxIntegerValue: Int = Int.MAX_VALUE
+    val maxDoubleValue: Double = Double.MAX_VALUE
+    val maxByteValue: Byte = Byte.MAX_VALUE
+    val maxShortValue: Short = Short.MAX_VALUE
+    val maxLongValue: Long = Long.MAX_VALUE
+    val maxFloatValue: Float = Float.MAX_VALUE
+
+//  Min Values for Integer Fata Types
+    val minIntegerValue: Int = Int.MIN_VALUE
+    val minDoubleValue: Double = Double.MIN_VALUE
+    val minByteValue: Byte = Byte.MIN_VALUE
+    val minShortValue: Short = Short.MIN_VALUE
+    val minLongValue: Long = Long.MIN_VALUE
+    val minFloatValue: Float = Float.MIN_VALUE
+
+    println(line)
+    println("Notes:")
+    println(line)
+    println("Variables:")
+    println(String.format("%-25s %s","Char", characterValue))
+    println(String.format("%-25s %s","String", stringValue))
+    println(String.format("%-25s %s","Boolean", booleanVale))
+    println(String.format("%-25s %,d","Integer", integerValue))
+    println(String.format("%-25s %.2f","Double", doubleValue))
+    println(String.format("%-25s %d","Byte", byteValue))
+    println(String.format("%-25s %d","Short", shortValue))
+    println(String.format("%-25s %d","Long", longValue))
+    println(String.format("%-25s %.2f","Float", floatValue))
+    println(line)
+    println("Max and Min Values for Integer Types:")
+    println(line)
+    println(String.format("%-25s %-25s %s","Data Type", "Min", "Max"))
+    println(String.format("%-25s %-25d %,d","Integer", minIntegerValue, maxIntegerValue))
+    println(String.format("%-25s %-25.2f %.2f","Double", minDoubleValue, maxDoubleValue))
+    println(String.format("%-25s %-25d %d","Byte", minByteValue, maxByteValue))
+    println(String.format("%-25s %-25d %d","Short", minShortValue, maxShortValue))
+    println(String.format("%-25s %-25d %d","Long", minLongValue, maxLongValue))
+    println(String.format("%-25s %-25.2f %.2f","Float", minFloatValue, maxFloatValue))
+    println(line)
 }
